a4e9703c38f36234009232a598af7b0b986fedfc
NR-91 <121296747+NR-91@users.noreply.github.com>
2023-03-04 21:03:52 +0200

    Update hw_1.py

--- hw_1.py
+++ hw_1.py
@@ -1,17 +1,17 @@
 import os,shutil
 import sys
-from pathlib import Path
 import re
 
-#  File Mask List
-IMAGES = ('JPEG', 'PNG', 'JPG', 'SVG') 
-DOCUMENTS = ('DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX') 
-VIDEOS = ('AVI', 'MP4', 'MOV', 'MKV') 
-SOUNDS = ('MP3', 'OGG', 'WAV', 'AMR') 
-ARCHIVES = ('ZIP', 'GZ', 'TAR', 'RAR') 
+#  key = Default Folder : List = File Mask 
 
-#  Default Folder List
-FOLDERS = ('/images', '/videos','/documents','/archives','/sounds', '/others') 
+FILE_MASK = {"/images":('JPEG', 'PNG', 'JPG', 'SVG') , 
+             "/documents":('DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX') ,
+             "/videos":('AVI', 'MP4', 'MOV', 'MKV') , 
+             "/sounds":('MP3', 'OGG', 'WAV', 'AMR'), 
+             "/archives":('ZIP', 'GZ', 'TAR', 'RAR') , 
+             "/others":() }
+
+
 
 # translation 
 CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
@@ -33,47 +33,50 @@ def normalise(name):
     name = rep.sub('_', name)
     return name
 
-# Create default folders
-def create_def_folder(current):
-    for d in FOLDERS:
-        def_dir = current + d
-        
-        isExist = os.path.exists(def_dir) 
-        if not isExist:
-            # Create a new directory because it does not exist
-            os.makedirs(def_dir) 
 
 # Sort function    
-def sort_file(file_dir, file_mask, curr_path, dest_path, f_m_c:bool):
+def sort_file(file_dir, file_mask, curr_path, dest_path):
+    
+    if len(file_mask) != 0:
+        f_m_c = True
+    else:
+        f_m_c = False
+        
     new_fn = []
     
     for file in file_dir:
-
         dest = ''
-
         if f_m_c == True:
             
             for ex in file_mask:
                 if file.endswith(ex.casefold()):
                     dest = curr_path + dest_path
                     new_fn = str(file).split('.')
-                
+                    
+                    if not os.path.exists(dest) :
+                        os.makedirs(dest) 
+                        
                     os.rename(curr_path + '\\' + file,curr_path + '\\' + translate(normalise(new_fn[0]))+'.'+ex.lower()) 
                     shutil.move(curr_path + '\\' + translate(normalise(new_fn[0]))+'.'+ex.lower() , dest)
                     break
         else:
             dest = curr_path + dest_path
+            if not os.path.exists(dest) :
+                os.makedirs(dest) 
+           
             if os.path.isfile(curr_path + '\\' + file ):
                 shutil.move(curr_path + '\\' + file , dest) 
-
+                
 # main           
 def main():
-    files = None
-    path_f = None
+    
+    files = ''
+    path_f = ''
     
     try:
         
         path_f = sys.argv[1]
+        
         if len(path_f) != 0:
             
             AppPath = sys.path[0]
@@ -82,53 +85,24 @@ def main():
             files=os.listdir(current)
             print(files)
             
-            print("Create folders...")
-            create_def_folder(current)
-            print(os.listdir(current))
-            
             print("Sorting the files...")
             
-            #Images
-            sort_file(files, IMAGES, current, FOLDERS[0],True) 
-            files_s = os.listdir(current + FOLDERS[0])
-            print(FOLDERS[0] + ' -->' , files_s)  
-            
-            #Videos
-            sort_file(files, VIDEOS, current, FOLDERS[1],True)
-            files_s = os.listdir(current + FOLDERS[1])
-            print(FOLDERS[1] + ' -->' , files_s)    
-            
-            #doc
-            sort_file(files, DOCUMENTS, current, FOLDERS[2],True)
-            files_s = os.listdir(current + FOLDERS[2])
-            print(FOLDERS[2] + ' -->' , files_s)    
-            
-            #arhive
-            sort_file(files, ARCHIVES, current, FOLDERS[3],True)
-            files_s = os.listdir(current + FOLDERS[3])
-            print(FOLDERS[3] + ' -->' , files_s)    
-            
-            #sounds
-            sort_file(files, SOUNDS, current, FOLDERS[4],True)
-            files_s = os.listdir(current + FOLDERS[4])
-            print(FOLDERS[4] + ' -->' , files_s)    
-            
-            #other
-            sort_file(files, () , current, FOLDERS[-1], False)
-            files_s = os.listdir(current + FOLDERS[-1])
-            print(FOLDERS[-1] + ' -->' , files_s)  
+            for k, v in FILE_MASK.items():
+                sort_file(files, v, current, k) 
             
             print("Sorting Completed...")
             files=os.listdir(current)
             
     except:
         
-        print('Path not found: ', path_f)
+        print('Path not found : ', path_f)
         print('example: scrypt.py /some_folder ')
         
     finally:
-        
-        print('Folders: ',files)
+        if len(path_f) != 0:
+            for i in os.listdir(current):
+                files_s = os.listdir(current + '/' + i)
+                print(i , ' -->', files_s)  
 		
 if __name__ == "__main__":
     main()
